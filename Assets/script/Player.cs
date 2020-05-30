2613cb5178e11417b70d7f2cdc40e6babc626935
JeewanthaLahiru <jeewanthalahiru00@gmail.com>
2020-05-30 18:13:06 +0530

    Added jump to the player controll script

--- Assets/script/Player.cs
+++ Assets/script/Player.cs
@@ -5,11 +5,31 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public float speed;
+    public float jumpSpeed = 8;
+    public Rigidbody rb;
+    public bool isGrounded = true;
 
     // Update is called once per frame
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
     void Update()
     {
         PlayerMovement();
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.AddForce(new Vector3(0, 5, 0), ForceMode.Impulse);
+            isGrounded = false;
+        }
+        
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "floor")
+        {
+            isGrounded = true;
+        }
     }
     void PlayerMovement()
     {
