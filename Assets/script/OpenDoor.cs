2daa3e0a311e696beafa566824151e71ea558ceb
JeewanthaLahiru <jeewanthalahiru00@gmail.com>
2020-06-13 14:28:08 +0530

    added a script to the door to open with E key

--- Assets/script/OpenDoor.cs
+++ Assets/script/OpenDoor.cs
@@ -8,16 +8,25 @@ public class OpenDoor : MonoBehaviour
     public GameObject TextDisplay;
     public float TheDistance = PlayerCast.DistanceFromTarget;
 
+    public GameObject TheDoor;
+
     void Update()
     {
         TheDistance = PlayerCast.DistanceFromTarget;
+        if (Input.GetButtonDown("Action"))
+        {
+            if (TheDistance <= 4)
+            {
+                StartCoroutine(OpenTheDoor());
+            }
+        }
     }
 
     void OnMouseOver()
     {
         if (TheDistance <= 4)
         {
-            TextDisplay.GetComponent<Text>().text = "Press Button";
+            TextDisplay.GetComponent<Text>().text = "E ඔබන්න ";
         }
 
     }
@@ -25,5 +34,16 @@ public class OpenDoor : MonoBehaviour
     void OnMouseExit()
     {
         TextDisplay.GetComponent<Text>().text = "";
+    }
+
+    IEnumerator OpenTheDoor()
+    {
+        TheDoor.GetComponent<Animator>().enabled = true;
+        yield return new WaitForSeconds(1);
+        TheDoor.GetComponent<Animator>().enabled = false;
+        yield return new WaitForSeconds(5);
+        TheDoor.GetComponent<Animator>().enabled = true;
+        yield return new WaitForSeconds(1);
+        TheDoor.GetComponent<Animator>().enabled = false;
     }
 }
