2cb603490d794dfe3cc93f36e056174a2157e883
JeewanthaLahiru <jeewanthalahiru00@gmail.com>
2020-06-13 13:34:23 +0530

    created a room and a door

---
+++ Assets/script/OpenDoor.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OpenDoor : MonoBehaviour
+{
+    public GameObject TextDisplay;
+    public float TheDistance = PlayerCast.DistanceFromTarget;
+
+    void Update()
+    {
+        TheDistance = PlayerCast.DistanceFromTarget;
+    }
+
+    void OnMouseOver()
+    {
+        if (TheDistance <= 4)
+        {
+            TextDisplay.GetComponent<Text>().text = "Press Button";
+        }
+
+    }
+
+    void OnMouseExit()
+    {
+        TextDisplay.GetComponent<Text>().text = "";
+    }
+}
